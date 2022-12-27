ea4cae691b76d2f8158fed0388d63c4b32d93158
NA <na@NAui-MacBookAir.local>
2022-12-27 11:41:53 +0900

    Add: 로그인 레이아웃 구현

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginModal from './pages/LoginModal/LoginModal';
 import Main from './pages/Main/Main';
 import ProductList from './pages/ProductList/ProductList';
 
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/productlist" element={<ProductList />} />
+        <Route path="/login" element={<LoginModal />} />
       </Routes>
     </BrowserRouter>
   );

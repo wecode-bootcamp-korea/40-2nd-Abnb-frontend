531ae2aa285ca891cf5a26e17e567071d767d994
NA <na@NAui-MacBookAir.local>
2022-12-29 17:13:42 +0900

    Add: 카드 리스트 레이아웃 및 무한 스크롤 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,62 @@
-import React from 'react';
+import Card from 'components/Card/Card';
+import React, { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
 
 const Main = () => {
-  return <h1>Abnb 메인 페이지 입니다</h1>;
+  const [roomList, setRoomList] = useState([]);
+  const observerTarget = useRef(null);
+  //TODO : Back API 구현 후 추가 구현 예정
+  useEffect(() => {
+    const observer = new IntersectionObserver(([targetnNode]) => {
+      if (targetnNode.isIntersecting) {
+        fetch('/data/MOCK.json')
+          .then(res => res.json())
+          .then(data => setRoomList(prev => [...prev, ...data]));
+      }
+    });
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <>
+      <ProductListContainer>
+        <ProductListBox>
+          {roomList.map(data => (
+            <Card key={data.id} {...data} />
+          ))}
+        </ProductListBox>
+      </ProductListContainer>
+      <div ref={observerTarget} />
+    </>
+  );
 };
 
 export default Main;
+
+const ProductListContainer = styled.div`
+  width: fit-content;
+  margin: 20px auto;
+`;
+
+const ProductListBox = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  position: relative;
+  width: 100%;
+  grid-gap: 25px;
+
+  @media (min-width: 670px) and (max-width: 1070px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (min-width: 1070px) and (max-width: 1400px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (min-width: 1400px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Card from 'components/Card/Card';
 
 const ProductList = () => {
-  return <h1>Abnb 리스트 페이지 입니다</h1>;
+  const [accommodationList, setAccommodationList] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/MOCK.json')
+      .then(res => res.json())
+      .then(data => setAccommodationList(data));
+  }, []);
+
+  return (
+    <ProductListContainer>
+      <ProductListBox>
+        {accommodationList.map(data => (
+          <Card key={data.id} {...data} />
+        ))}
+      </ProductListBox>
+    </ProductListContainer>
+  );
 };
 
 export default ProductList;
+
+const ProductListContainer = styled.div`
+  width: fit-content;
+  margin: 20px auto;
+`;
+const ProductListBox = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  position: relative;
+  width: 100%;
+  grid-gap: 25px;
+
+  @media (min-width: 670px) and (max-width: 1070px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (min-width: 1070px) and (max-width: 1400px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (min-width: 1400px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
